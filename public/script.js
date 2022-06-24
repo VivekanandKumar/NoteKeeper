7e1d3d7b096e2f33a26af138d960718f2ee23b49
Vivekanand Kumar <mailofvivekanand@gmail.com>
2022-06-24 14:53:57 +0530

    initial commit

--- public/script.js
+++ public/script.js
@@ -34,16 +34,16 @@ $(document).ready(function () {
     const description = $(this).children("article").text();
     $(".modify-note input").val($.trim(title));
     $(".modify-note textarea").val($.trim(description));
-    $(".modify-note").toggleClass("hidden");
+    $(".modify-note").toggleClass("hideme");
   });
   $(".new-note").click(function () {
-    $(".add-note").toggleClass("hidden");
+    $(".add-note").toggleClass("hideme");
   });
   $(".add-note .close").click(function () {
-    $(".add-note").toggleClass("hidden");
+    $(".add-note").toggleClass("hideme");
   });
   $(".modify-note .close").click(function () {
-    $(".modify-note").toggleClass("hidden");
+    $(".modify-note").toggleClass("hideme");
   });
 
   $(".edit-btn").click(function () {
@@ -154,7 +154,7 @@ function editNote(id, data) {
   })
     .then((response) => {
       display();
-      $(".modify-note").addClass("hidden");
+      $(".modify-note").toggleClass("hideme");
       return notyf.success(response.message);
     })
     .catch((error) => {
@@ -169,7 +169,7 @@ function deleteNote(id) {
   })
     .then((response) => {
       display();
-      $(".modify-note").addClass("hidden");
+      $(".modify-note").toggleClass("hideme");
       return notyf.success(response.message);
     })
     .catch((error) => {
@@ -186,7 +186,7 @@ function newNote(data) {
   })
     .then((response) => {
       display();
-      $(".add-note").addClass("hidden");
+      $(".add-note").toggleClass("hideme");
       return notyf.success(response.message);
     })
     .catch((error) => {
